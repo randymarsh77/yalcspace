3294aada576ba866bce2460f32d4ace15beeb442
randymarsh77 <mattpsbs@gmail.com>
2024-07-01 08:09:08 -0700

    fix(logging): Log error of failed commands.

--- src/compatibility.ts
+++ src/compatibility.ts
@@ -7,12 +7,15 @@ export function toPlatformPath(path: string) {
 
 export function runCommand(command: string, args: string[], options?: SpawnSyncOptions) {
 	var isWindows = process.platform === 'win32';
-	const { status } = spawnSync(command, args, {
+	const { status, error } = spawnSync(command, args, {
 		stdio: ['ignore', 'ignore', 'inherit'],
 		shell: isWindows, // Only include true for Windows because it can include runtime breaking behavior on macOS.
 		...options,
 	});
 	if (status !== 0) {
+		if (error) {
+			console.error(error);
+		}
 		throw new Error(
 			`Command failed: ${command + (args.length > 0 ? ' ' + args.join(' ') : '')} | From: ${
 				options?.cwd ?? process.cwd()
